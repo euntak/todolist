b2c9b5182a23348ff7355fd5157b7e1ffe6b73c4
류성탁 <tak@lyuseongtag-ui-MacBook-Air.local>
2017-06-05 00:59:42 +0900

    add front-end

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,6 +1,202 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	var ENTER_KEY = 13;
+	var todos = [];
+	var filters = '/all';
+
+	var Todo = {
+
+		init: function () {
+			this.getAllList();
+			this.bindEvent();
+		},
+		bindEvent: function () {
+			$('.todoapp input.new-todo').on('keyup', this.addTodo);
+			$('#todo-list').on('change', '.toggle', this.toggle)
+				.on('click', '.destroy', this.removeTodo);
+
+			$('footer ul.filters li a').on('click', this.setFilters);
+			$('footer button.clear-completed').on('click', this.removeCompletedlist);
+		},
+		getTodoTemplete: function (t) {
+			return (t.completed
+				? '<li class="completed" data-id=' + t.id + '>'
+				: '<li data-id=' + t.id + '>')
+				+ '<div class="view">'
+				+ '<input class="toggle" type="checkbox"' + (t.completed ? 'checked' : '') + '>'
+				+ '<label>' + t.todo + '</label>'
+				+ '<button class="destroy"></button>'
+				+ '</div>'
+				+ '<input class="edit" value="' + t.todo + '">'
+				+ '</li>';
+		},
+		getAllList: function () {
+			var result = this.todoApi('GET', 'api/todos');
+			result.done(function (res) {
+				todos = res;
+				res.map(function (t) {
+					var html = Todo.getTodoTemplete(t);
+					$('ul.todo-list').prepend(html);
+				});
+
+				Todo.updateActiveTodoListCount();
+			})
+		},
+		toggle: function (event) {
+			event.preventDefault();
+
+			var $list = $(this).closest('li');
+			var $checkbox = $list.find('input[type="checkbox"]');
+
+			var id = $list.data('id');
+			var index = Todo.getIndexOfArray(id);
+
+			todos[index].completed = !todos[index].completed;
+
+			var data = { 'id': id, 'completed': ((todos[index].completed) ? 1 : 0) };
+			var result = Todo.todoApi('PUT', '/api/todos/', JSON.stringify(data));
+
+			result.then(function (res) {
+				if (res) {
+					if ($list.hasClass('completed')) {
+						$checkbox.attr('checked', false);
+						$list.removeClass('completed');
+					} else {
+						$checkbox.attr('checked', true);
+						$list.addClass('completed');
+					}
+				}
+				Todo.rendering();
+			});
+		},
+		addTodo: function (event) {
+			event.preventDefault();
+
+			var $input = $(event.target);
+			var val = $input.val().trim();
+
+			if (event.which !== ENTER_KEY || !val) {
+				return;
+			}
+
+			var result = Todo.todoApi('POST', '/api/todos', JSON.stringify({ todo: val }));
+			result.then(function (res) {
+				todos.push({
+					id: res,
+					completed: false,
+					todo: val,
+				});
+				$input.val('');
+				Todo.rendering();
+			})
+		},
+		getActiveList: function () {
+			return todos.filter(function (todo) {
+				return !todo.completed;
+			});
+		},
+		getCompletedList: function () {
+			return todos.filter(function (todo) {
+				return todo.completed;
+			});
+		},
+		setFilters: function (event) {
+			event.preventDefault();
+
+			var $element = $(this);
+			var currentFilter = $element.attr('href').split('/')[1];
+			console.log(currentFilter);
+
+			// Remove all
+			$('.selected').removeClass('selected');
+			// Add to current filters
+			$element.addClass('selected');
+
+			if (currentFilter === 'active') filters = 'Active';
+			else if (currentFilter === 'completed') filters = 'Completed';
+			else filters = 'All';
+
+			Todo.getFilteredTodoList();
+			Todo.rendering();
+		},
+		getIndexOfArray: function (id) {
+			for (var i = 0; i < todos.length; i++) {
+				if (todos[i].id === id) return i;
+			}
+		},
+		removeTodo: function (event) {
+			var id = $(this).closest('li').data('id');
+			var deleteTodoId = Todo.getIndexOfArray(id);
+			var requestURL = '/api/todos/' + id;
+
+			// server 
+			var result = Todo.todoApi('DELETE', requestURL);
+			result.then(function (res) {
+				if (res) {
+					todos.splice(deleteTodoId, 1);
+				} else {
+					console.log('failure to delete todo item');
+				}
+				Todo.rendering();
+			});
+		},
+		todoApi: function (method, url, data = null) {
+			return $.ajax({
+				contentType: 'application/json; charset=UTF-8',
+				method: method,
+				url: url,
+				data: data,
+				dataType: 'json',
+			});
+		},
+		getFilteredTodoList: function () {
+			if (filters === 'Active') {
+				return Todo.getActiveList();
+			}
+
+			if (filters === 'Completed') {
+				return Todo.getCompletedList();
+			}
+
+			return todos;
+		},
+		rendering: function (event) {
+			var filteredTodo = Todo.getFilteredTodoList();
+			
+			// clear all list 
+			$('ul.todo-list').html('');
+
+			Todo.updateActiveTodoListCount();
+
+			// rendering
+			filteredTodo.map(function (t) {
+				var html = Todo.getTodoTemplete(t);
+				$('ul.todo-list').prepend(html);
+			});
+
+			$(".new-todo").focus();
+		},
+		updateActiveTodoListCount: function () {
+			var count = 0;
+			todos.map(function (todo) {
+				if (!todo.completed) count++;
+			});
+
+			$('.footer .todo-count strong').html(count);
+
+			return count;
+		},
+		removeCompletedlist: function () {
+			var result = Todo.todoApi('DELETE', '/api/todos');
+
+			result.then(function (res) {
+				todos = Todo.getActiveList();
+				Todo.rendering();
+			});
+		}
+	};
+
+	Todo.init();
 
 })(window);
